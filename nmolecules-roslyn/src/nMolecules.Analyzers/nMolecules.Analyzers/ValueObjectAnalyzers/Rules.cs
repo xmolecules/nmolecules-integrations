9903e0a2a170c8278a85034c8ca9762359ba9fd2
Lars Baldes <lb@wps.de>
2022-06-11 12:09:12 +0200

    added sample .editorconfig

--- nmolecules-roslyn/src/nMolecules.Analyzers/nMolecules.Analyzers/ValueObjectAnalyzers/Rules.cs
+++ nmolecules-roslyn/src/nMolecules.Analyzers/nMolecules.Analyzers/ValueObjectAnalyzers/Rules.cs
@@ -4,13 +4,13 @@ namespace NMolecules.Analyzers.ValueObjectAnalyzers
 {
     public static class Rules
     {
-        public const string ValueObjectsMustImplementIEquatableId = "XMoleculesValueObject0001";
-        public const string ValueObjectsMustBeSealedId = "XMoleculesValueObject0002";
-        public const string NoEntitiesInValueObjectsId = "XMoleculesValueObject0003";
-        public const string NoServicesInValueObjectsId = "XMoleculesValueObject0004";
-        public const string NoRepositoriesInValueObjectsId = "XMoleculesValueObject0005";
-        public const string NoAggregateRootsInValueObjectsId = "XMoleculesValueObject0006";
-        public const string ValueObjectsMustBeImmutableId = "XMoleculesValueObject0007";
+        public const string NoEntitiesInValueObjectsId = "XMoleculesValueObject0001";
+        public const string NoServicesInValueObjectsId = "XMoleculesValueObject0002";
+        public const string NoRepositoriesInValueObjectsId = "XMoleculesValueObject0003";
+        public const string NoAggregateRootsInValueObjectsId = "XMoleculesValueObject0004";
+        public const string ValueObjectsMustBeImmutableId = "XMoleculesValueObject0005";
+        public const string ValueObjectsMustImplementIEquatableId = "XMoleculesValueObject1001";
+        public const string ValueObjectsMustBeSealedId = "XMoleculesValueObject1002";
 
         public static readonly DiagnosticDescriptor ValueObjectMustNotUseEntityRule = new(NoEntitiesInValueObjectsId,
             new LocalizableResourceString(nameof(Resources.ValueObjectUsesEntityTitle),
